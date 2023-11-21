0fcaa088f976b679ef21e8b6be078208ad1018dc
Aanis Nuur <Aanisn10@gmail.com>
2023-11-21 14:58:15 +0000

    completed the testing for sum

--- task_jest_testing/calculator_testing_start/calculator.test.js
+++ task_jest_testing/calculator_testing_start/calculator.test.js
@@ -10,22 +10,26 @@ const {
 
 describe('sum', () => { 
   
-  test('can add two small positive numbers', () => {
+  test.skip('can add two small positive numbers', () => {
     expected = 5;
     actual = sum(2, 3);
     expect(actual).toBe(expected);
   });
 
   test.skip('can add two large positive numbers', () => { 
-    
+    expected = 100000;
+    actual = sum(20000,80000);
+    expect(actual).toBe(expected);
   });
 
   test.skip('can add two negative numbers', () => {
-    
+    expected = -10;
+    actual = sum (-7,-3);
+    expect(actual).toBe(expected);
   });
 
   test.skip('can add zero', () => {
-    
+    expect(sum(0,0)).toBe(0);
   });
 
 });
